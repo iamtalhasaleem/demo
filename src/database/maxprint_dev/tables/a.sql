4192fbeacf15c5bc1d1210ab045f752c014afb26
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2026-05-14 10:49:20 +0000

    Initialize dev branch with baseline export

---
+++ src/database/maxprint_dev/tables/a.sql
@@ -0,0 +1,6 @@
+create table maxprint_dev.a (
+    a number(1, 1)
+);
+
+
+-- sqlcl_snapshot {"hash":"dfd5ae0720a7b239556ccb0229276228ad14d37c","type":"TABLE","name":"A","schemaName":"MAXPRINT_DEV","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>MAXPRINT_DEV</SCHEMA>\n   <NAME>A</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>A</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            <PRECISION>1</PRECISION>\n            <SCALE>1</SCALE>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
